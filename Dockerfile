c7b6db48e8bf0bd7cb24f72a4cce635fff886460
Samir L. Boulema <sboulema@gmail.com>
2017-11-29 13:53:25 +0100

    Use NPM

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,14 @@
+# First Stage
+FROM monostream/nodejs-gulp-bower
+
+COPY . /workspace
+
+RUN npm install
+RUN gulp build
+
+# Second Stage
 FROM nginx
 
-COPY . /usr/share/nginx/html/
+COPY --from=0 /workspace/dist /usr/share/nginx/html/
 
 EXPOSE 80
---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var gulp = require('gulp');
+
+gulp.task('scripts', function () {
+  gulp.src('node_modules/leaflet-measure/dist/*.js').pipe(gulp.dest('dist/js'));
+  gulp.src('node_modules/leaflet-measure/dist/*.css').pipe(gulp.dest('dist/css'));
+  gulp.src('node_modules/leaflet-measure/dist/images/*').pipe(gulp.dest('dist/css/images'));
+
+  gulp.src('node_modules/leaflet-measure-path/*.js').pipe(gulp.dest('dist/js'));
+  gulp.src('node_modules/leaflet-measure-path/*.css').pipe(gulp.dest('dist/css'));
+
+  gulp.src('node_modules/leaflet.markercluster/dist/*.js').pipe(gulp.dest('dist/js'));
+  gulp.src('node_modules/leaflet.markercluster/dist/*.css').pipe(gulp.dest('dist/css'));
+
+  gulp.src('node_modules/togeojson/*.js').pipe(gulp.dest('dist/js'));
+
+  gulp.src('node_modules/leaflet-geosearch/dist/*.js').pipe(gulp.dest('dist/js'));
+
+  gulp.src('node_modules/tokml/*.js').pipe(gulp.dest('dist/js'));
+
+  gulp.src('node_modules/wkx/dist/*.js').pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('copy', function () {
+  gulp.src('img/*').pipe(gulp.dest('dist/img'));
+  gulp.src('js/*').pipe(gulp.dest('dist/js'));
+  gulp.src('favicon.ico').pipe(gulp.dest('dist'));
+  gulp.src('index.html').pipe(gulp.dest('dist'));
+});
+
+gulp.task('build', function () {
+  gulp.start('scripts');
+  gulp.start('copy');
+});
